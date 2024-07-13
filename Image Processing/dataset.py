377242776afbe068dbdf78b0b70feec0b064b74a
MinavKaria <minavpkaria@gmail.com>
2024-07-13 20:20:17 +0530

    chore: Add image processing code and dataset

--- Image Processing/dataset.py
+++ Image Processing/dataset.py
@@ -8,29 +8,32 @@ from PIL import Image
 import numpy as np
 
 def pil_to_0_1_array(image):
-    # Convert image to grayscale (mode 'L')
+
     image_gray = image.convert('L')
-    
-    # Resize image to 28x28 pixels (if needed)
     image_resized = image_gray.resize((28, 28))
-    
-    # Convert PIL image to NumPy array
     image_array = np.array(image_resized)
-    
-    # Apply thresholding to convert pixel values to 0 or 1
     threshold = 128
     image_array_binary = (image_array > threshold).astype(np.uint8)
-    
-    return image_array_binary.flatten().tolist()  # Return flattened list of 0s and 1s
+    return image_array_binary.flatten().tolist() 
 
 # Example usage:
 # Assuming img is a PIL image object
 
-test=ds['train']['image'][0]
-label=ds['train']['label'][0]
-image_data = pil_to_0_1_array(test)
-print(image_data)
-print(label)
+# print(ds)
+
+# train=ds['train']['image'][0]
+# array=pil_to_0_1_array(train)
+# print(len(array))
+
+print(ds['train'].shape(0))
+
+
+
+# test=ds['train']['image'][0]
+# label=ds['train']['label'][0]
+# image_data = pil_to_0_1_array(test)
+# print(image_data)
+# print(label)
 
 
 
